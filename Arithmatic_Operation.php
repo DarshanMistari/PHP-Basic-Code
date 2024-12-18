f1c4d8e1e2648f37fd875f5f5b67efa57c189b29
DarshanMistari <mistaridarshan2@gmail.com>
2024-12-18 08:55:07 +0530

    [ADDED]:Darshan - PHP code Arithmatic Operation

---
+++ Arithmatic_Operation.php
@@ -0,0 +1,16 @@
+<?php
+$a = 20;
+$b = 5;
+
+$sum = $a + $b;
+$sub = $a - $b;
+$div = $a / $b;
+$mul = $a * $b;
+
+echo("Addition :$sum  <br>");
+echo("Subtractio :$sub  <br>");
+echo("Division :$div  <br>");
+echo("Multiplication :$mul");
+
+
+?>
