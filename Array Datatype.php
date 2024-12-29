ed065331ef43dd4e9bd3acfca0e42c7a8dfeca12
DarshanMistari <mistaridarshan2@gmail.com>
2024-12-29 08:54:14 +0530

    [ADDED]:Darshan - PHP code Array datatype

---
+++ Array Datatype.php
@@ -0,0 +1,12 @@
+<?php
+
+$bikes = array ("Royal Enfield","Yamaha");
+var_dump($bikes);   //the var_dump() function return the datatype and values
+
+echo"</br><br>";
+
+echo"Array Element1 : $bikes[0]  <br><br>";
+
+echo"Array Element2 : $bikes[1] <br><br>";
+
+?>
