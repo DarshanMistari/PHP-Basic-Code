43ec703db1f2ec91aec9d1ac63d57c42e676ae53
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-04 09:05:15 +0530

    [ADDED]:Darshan - PHP code Associated demo

---
+++ Associative Demo.php
@@ -0,0 +1,9 @@
+\\Associative Array\\
+<?php
+$arr=array("Darshan"=>"100000","Ravivdra"=>"50000","Kalpesh"=>"40000","Ravindra"=>"100000");
+echo $arr["Darshan"] ."<br>";
+echo $arr["Ravindra"] ."<br>";
+echo $arr["Kalpesh"] ."<br>";
+echo $arr["Ravindra"] ."<br>";
+ 
+?>
