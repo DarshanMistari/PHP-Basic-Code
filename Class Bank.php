24021d24c108b793946fc0f5a26451cb3936ae8c
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-06 10:17:41 +0530

    [ADDED]:Darshan - PHP code classs back

---
+++ Class Bank.php
@@ -0,0 +1,36 @@
+<?php
+class bank
+{
+private $accno;
+private $balance;
+
+public function newaccno($a,$b)
+{
+$this->accno=$a;
+$this->balance=$b;
+}
+public function deposite($x)
+{
+$this->balance=$this->balance+$x;
+}
+public function withdraw($y)
+{
+$this->balance=$this->balance-$y;
+}
+public function viewbalance()
+{
+echo"<br>Account No is:".$this->accno;
+echo"<br>Bank Balance is :".$this->balance;
+}
+}
+
+$cust1=new bank;
+$cust1->newaccno(1234,5000);
+$cust1->deposite(10000);
+$cust1->withdraw(5000);
+$cust1->viewbalance();
+
+
+
+
+
