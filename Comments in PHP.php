3467479d31724597276cea6a0f14fffbf1e700f9
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-07 09:31:58 +0530

    [ADDED]:Darshan - PHP code comments

---
+++ Comments in PHP.php
@@ -0,0 +1,11 @@
+<?php 
+//this is C++ style single line comment
+#this is Unix shell style single line comment
+
+echo"Welcome to PHP Single line Comments <br>";
+
+/*  Anything placed within comment will not be displayed on the browser  */
+
+echo" <br> Welcome to PHP Multiple line Comments";
+
+?>
