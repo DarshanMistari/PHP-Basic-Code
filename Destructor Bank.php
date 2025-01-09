65a406ac763e754549710dcba7a79504a1013776
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-09 19:06:44 +0530

    [ADDED]:Darshan - PHP destructor bank

---
+++ Destructor Bank.php
@@ -0,0 +1,39 @@
+<?php
+class bank
+{
+private $accno;
+private $balance;
+
+public function newaccno($a,$b)
+{
+$this->accno=$a;
+$this->balance=$b;
+}
+public function deposite($x)
+{
+$this->balance=$this->balance+$x;
+}
+public function withdraw($y)
+{
+$this->balance=$this->balance-$y;
+}
+public function viewbalance()
+{
+echo"<br>Account No is:".$this->accno;
+echo"<br>Bank Balance is :".$this->balance;
+}
+public function_bank()
+{
+$this->accno=0;
+$this->balance=0; 
+}
+
+}
+
+$cust1=new bank;
+$cust1->newaccno(1234,5000);
+$cust1->deposite(10000);
+$cust1->withdraw(5000);
+$cust1->viewbalance();
+
+?>
