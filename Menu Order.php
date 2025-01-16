d7742c6423c89c800145b77858938c5df514e790
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-16 08:39:09 +0530

    [ADDED]:Darshan - PHP code menu order

---
+++ Menu Order.php
@@ -0,0 +1,81 @@
+<?php
+$num=$_REQUEST['txtno1'];
+$quant=$_REQUEST['txtno2'];
+$bill;
+
+echo"*** MENU ***";
+echo"\n1.Pizza";
+echo"\n2.Berger";
+echo"\n3.Sandwich";
+echo"\n4.Pasta";
+echo"\n5.Crime Role";
+echo"\n6.Kaju Shake";
+echo"\n7.Badam Shake";
+echo"\8.Falooda shake";
+echo"\n9.Ice Cream";
+echo"\n.10.Exit";
+
+if($num==1)
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza";
+$bill=quant*150;
+echo"Total Bill of Pizza is $bill Rs."
+}
+if else
+{
+echo"Your order is Sandwich with Price 120 Rs.\n";
+echo"Enter the Quantity of Sandwich";
+bill=quant*120;
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+if else
+{
+
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+if else
+{
+
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+if else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+else
+{
+echo"Your order is Pizza with Price 150 Rs.\n";
+echo"Enter the Quantity of Pizza:";
+bill=quant*150;
+}
+?>
