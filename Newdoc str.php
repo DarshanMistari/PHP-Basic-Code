70754e06fb037d8e66fc05f24824e5b9f425408c
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-17 19:10:45 +0530

    [ADDED]:Darshan - PHP code newdoc str

---
+++ Newdoc str.php
@@ -0,0 +1,12 @@
+<?php 
+
+$str= <<<'DEMO' Welcome to InfoGalaxy.Learn with newdoc Example.DEMO;
+
+echo $str;
+
+echo"<br> <br>";
+
+echo<<< 'DEMO';    //Here we are not storing string content in variable str. 
+Welcome to InfoGalaxy.Learn with newdoc example.Demo;
+ 
+?>
