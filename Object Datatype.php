307228f27ce494747f0bfc2f1daabbf6753ce651
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-17 19:12:32 +0530

    [ADDED]:Darshan - PHP code object datatype

---
+++ Object Datatype.php
@@ -0,0 +1,17 @@
+<?php
+
+class bike 
+{
+   function model ()
+   {
+     $model_name = "Royal Enfield";
+     
+     echo"Bike Model :".$model_name;
+   }
+
+}
+
+$obj = new bike();
+$obj ->model();
+
+?>
