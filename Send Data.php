6c137a50100fd93ff399b7f9b11e3e359d308b39
DarshanMistari <mistaridarshan2@gmail.com>
2025-01-18 08:36:44 +0530

    [ADDED]:Darshan - PHP code send data

---
+++ Send Data.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Data send the Variable </tilte>
+</head>
+<body>
+<h1> Data Send the Variable <h1>
+<p> click the link Below thouth the url </p>
+<a href="receiver.php?name=Darshan&age=19">send Data</a>
+</body>
+</html>
